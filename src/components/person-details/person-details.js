1fd5b619ded9cbee9bf3ada252315d1fd811756c
Jodar Nimatov <zodarbekn@gmail.com>
2022-12-26 01:10:16 +0600

    person, planet Details

---
+++ src/components/person-details/person-details.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service'
+import './person-details.css';
+
+export default class PersonDetails extends Component {
+
+  swapi = new SwapiService()
+
+  state = {
+    data: {}
+  }
+
+  _updatePerson = (id) => {
+    this.swapi.getPerson(id)
+    .then(data => {
+      this.setState({data: data})
+    })
+  }
+
+  componentDidMount = () => {
+
+    setInterval(() => {
+      const id = Math.floor(Math.random() * (25 - 1 + 1) + 1)
+      this._updatePerson(id)
+    }, 3000)
+  }
+
+
+
+  render() {
+
+
+    
+
+    const {id, name, gender, birthYear, eyeColor} = this.state.data;
+    const personImg = `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`
+
+    return (
+      <div className="person-details card">
+        <img className="person-image"
+          src={personImg}/>
+
+        <div className="card-body">
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Gender</span>
+              <span>{gender}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Birth Year</span>
+              <span>{birthYear}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Eye Color</span>
+              <span>{eyeColor}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+import './random-planet.css';
+
+export default class RandomPlanet extends Component {
+swapi = new SwapiService()
+
+state = {
+  data: {}
+}
+
+_updatePlanet = (id) => {
+  this.swapi.getPlanet(id)
+  .then(data => {
+    this.setState({data: data})
+  })
+}
+
+componentDidMount = () => {
+
+  setInterval(() => {
+    const id = Math.floor(Math.random() * (20 - 1 + 1) + 1)
+    this._updatePlanet(id)
+  }, 3000)
+ 
+
+}
+
+
+  render() {
+
+    const {id, name, population, rotationPeriod, diameter} = this.state.data ;
+    const planetImg = `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`
+    return (
+      <div className="random-planet jumbotron rounded">
+        <img className="planet-image"
+             src={planetImg}/>
+        <div>
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population</span>
+              <span>{population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter</span>
+              <span>{diameter}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    );
+  }
+}
